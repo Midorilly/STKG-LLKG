89fea68eaf319c0b4e87e848e9053b0ff27b5363
Midorilly <angelica.ghizzota@gmail.com>
2024-04-12 11:50:41 +0200

    Implement Wikidata query

--- src/queries.py
+++ src/queries.py
@@ -1,4 +1,4 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, SPARQLExceptions
 from rdflib import Graph
 from namespaces import *
 from rdflib.namespace import RDF, RDFS, OWL, XSD, DCTERMS
@@ -6,6 +6,7 @@ import rdflib.query
 import urllib.error
 import time
 import logging
+import socket
 
 q = Graph()
 q.bind("rdf", RDF)
@@ -36,23 +37,6 @@ q.bind("dummy", DUMMY)
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-MAXRETRY = 5
-def queryRetry(query: str, initNs, initBindings) -> rdflib.query.Result:
-    backoff = 0
-    for i in range(MAXRETRY):
-        try:
-            #print('Query tentative {}...'.format(i))
-            result = q.query(query, initNs = initNs, initBindings = initBindings)
-            result.bindings
-            return result
-        except urllib.error.URLError or TimeoutError as e:
-            if i == MAXRETRY-1:
-                raise e
-            backoffS = 2 ** backoff
-            backoff += 1
-            logger.info('Network error {} during query, waiting for {}s and retrying'.format(e, backoffS))
-            time.sleep(backoffS)
-
 lemmaQuery = (""" SELECT ?lemma 
     WHERE {
         SERVICE <https://lila-erc.eu/sparql/lila_knowledge_base/sparql> {
@@ -70,13 +54,48 @@ senseQuery = '''SELECT ?senseURI
         }}
     }}'''
 
-documentQuery = '''SELECT ?document
+documentQuery = '''SELECT ?documentURI ?languageISO
     WHERE {{
-        VALUES ?title {{ "{}"@la}}
-        ?document wdt:P31 wd:Q7725634 ;
-           wdt:P1476 ?title.
-      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}   
-    }} ''' 
+        BIND (wd:{} AS ?authorURI)
+        ?documentURI wdt:P50 ?authorURI .
+        ?authorURI wdt:P6886 ?language.
+        ?language wdt:P220 ?languageISO .
+        {{ ?documentURI <http://www.w3.org/2004/02/skos/core#altLabel> ?label ;
+            wdt:P407 ?language }}
+        UNION
+        {{ ?documentURI <http://www.w3.org/2000/01/rdf-schema#label> ?label ;
+            wdt:P407 ?language }} 
+    FILTER (regex(str(?label), "{}", "i"))
+    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+    }} LIMIT 1
+'''
+
+authorQuery = '''SELECT ?authorURI 
+    WHERE {{
+        ?authorURI wdt:P31 wd:Q5 .
+        {{ ?authorURI <http://www.w3.org/2004/02/skos/core#altLabel> ?label .}}
+        UNION
+        {{ ?authorURI <http://www.w3.org/2000/01/rdf-schema#label> ?label . }}
+    FILTER (regex(str(?label), "{}", "i"))
+    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+    }} LIMIT 1
+'''
+
+MAXRETRY = 5
+def queryRetry(query: str, initNs, initBindings) -> rdflib.query.Result:
+    backoff = 1
+    for i in range(MAXRETRY):
+        try:
+            result = q.query(query, initNs = initNs, initBindings = initBindings)
+            result.bindings
+            return result
+        except urllib.error.URLError or TimeoutError as e:
+            if i == MAXRETRY-1:
+                raise e
+            backoffS = 2 ** backoff
+            backoff += 1
+            logger.info('{} \nwaiting for {}s and retrying'.format(e, backoffS))
+            time.sleep(backoffS)
 
 def transform2dicts(results):
     new_results = []
@@ -88,10 +107,25 @@ def transform2dicts(results):
     return new_results
 
 def query(queryString):
+    socket.getaddrinfo('localhost',8080)
     endpoint = "https://query.wikidata.org/sparql"
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(queryString)
     sparql.setReturnFormat(JSON)
+    results = []
+    n = 2
+    for i in range(MAXRETRY):
+        try:
+            results = sparql.queryAndConvert()['results']['bindings']
+            results = transform2dicts(results)
+            return results
+        except urllib.error.HTTPError or SPARQLExceptions.EndPointInternalError or urllib.error.URLError as e:
+            if i == MAXRETRY-1:
+                raise e
+            else:
+                logger.info('{}, waiting 60s'.format(e))
+                backoffS = n * 61
+                n += 1
+                time.sleep(backoffS)        
 
-    results = sparql.queryAndConvert()['results']['bindings']
-    results = transform2dicts(results)
+    
